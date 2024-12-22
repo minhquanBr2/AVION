37921234aeca15fde9d5b957fcd24a4f27eca557
minhquanBr2 <minhquanhole03@gmail.com>
2024-12-22 21:52:22 +0700

    addd

--- avion/models/utils.py
+++ avion/models/utils.py
@@ -62,6 +62,7 @@ def remap_keys_from_open_clip_to_vit(
         if key in ["visual.proj", "text_projection", "logit_scale"]:
             continue
         if use_fast_conv1 and key == 'visual.conv1.weight':
+            print(f"use_fast_conv1")
             remapped_state_dict['visual.conv1.weight'] = clip_state_dict[key].flatten(1)
             # remapped_state_dict['visual.conv1.weight'] = clip_state_dict[key]
             shape = remapped_state_dict['visual.conv1.weight']
